234e2cbc31289bfe207af5c95cc84d87f2644d0a
AngelReina <113972511+AngelReina@users.noreply.github.com>
2022-10-20 09:38:33 +0200

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,26 @@
 # Ejercicio 2
 '''
 Crea una lista e inicializala con 5 cadenas de caracteres leídas por teclado. 
-Copia los elementos de la lista en otra lista pero en orden inverso, y muestra sus elementos por la pantalla.
+Copia los elementos de la lista en otra lista pero en orden inverso,
+y muestra sus elementos por la pantalla.
 '''
+# Función de La lista de Caracteres
 
+def listaDeCaracter():
+    lst=[]
+    for i in range(0,5):
+       caracter= str(input("Dime un caracter: "))
+       lst.append(caracter)
+    return lst
+
+# Función de La lista de Caracteres Inversa
+
+def listaDeCaracterInversa(lst):
+    lst.reverse()
+    return lst
+
+# Muestro el resultado
+
+lista=listaDeCaracter()
+print(lista)
+print(listaDeCaracterInversa(lista))
