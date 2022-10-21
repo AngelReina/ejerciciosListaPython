c6c9ba4f9eb0105ef6f9f50bf6892258be1cb3e9
AngelReina <113972511+AngelReina@users.noreply.github.com>
2022-10-21 11:10:07 +0200

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,24 @@
 # Ejercicio 4 #
 '''
-Programa que declare una lista y la vaya llenando de núemros hasta que introducimos un número negativo.
+Programa que declare una lista y la vaya llenando
+de núemros hasta que introducimos un número negativo.
 Entonces se debe imprmir el vector (sólo los elementos introducidos)
 '''
+# número infinito
+positive_infnity = float('inf')
+# Lista de todas los números
+def listaTotal():
+    lst=[]
+    while(positive_infnity>0):
+        try:
+            num= int(input("Dime un número: "))
+            if(num>=0):
+               lst.append(num)
+            else:
+                print("Has introducido un número negativo")
+                break
+        except:
+            print("Tiene que ser un número")
+    return lst
+#Muestro la lista total de todos los números
+print(listaTotal())
