a1e88fe3ff0781015866780e1fdfbb9b3eea9eb3
AngelReina <113972511+AngelReina@users.noreply.github.com>
2022-10-25 10:05:37 +0200

    Update ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,5 +1,4 @@
 # Ejercicio 7 #
-# Ejercicio 7 #
 '''
 Programa que declare tres listas 'lista1', 'lista2' de cinco enteros cada uno. 
 Calcule lista3 = lista1 + lista2, por ejemplo 
@@ -11,17 +10,10 @@ Calcule lista3 = lista1 + lista2, por ejemplo
 '''
 #Importo el random
 import random
-# Lista 1
-def lista_1():
+# Creo una funcion de listas randoms
+def listasRandom(n):
     lst=[]
-    for i in range(0,5):
-        num_random=random.randint(0,10)
-        lst.append(num_random)
-    return lst
-# Lista 2
-def lista_2():
-    lst=[]
-    for i in range(0,5):
+    for i in range(0,n):
         num_random=random.randint(0,10)
         lst.append(num_random)
     return lst
@@ -31,11 +23,13 @@ def lista_3(lst,lst2):
     for i in range (len(lst)):
         lst_3.append(lst[i]+lst2[i])
     return lst_3
+# Hago la lista 1
+lista=listasRandom(5)
 # Muestro la lista 1
-lista=lista_1()
 print("  ",lista)
+# Hago la lista 2
+lista2=listasRandom(5)
 # Muestro la lista 2
-lista2=lista_2()
 print("+ ",lista2)
 # Decoración
 print("--------------------")
