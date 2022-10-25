516cfb9e348e9e91ea189f3b510969e029a82333
AngelReina <113972511+AngelReina@users.noreply.github.com>
2022-10-25 09:25:08 +0200

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -7,11 +7,14 @@ el nombre del mes. Debes usar listas. Nota: En una lista
 podemos guardar listas.
 Para simplificarlo vamos a suponer que febrero tiene 28 días
 '''
+# Lista de los meses
 lst_meses=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+# Lista de los días
 lst_dias=[31,28,31,30,31,30,31,31,30,31,30,31]
-
+# Pido el numero del mes
 mes = int(input("Introduce un mes (1-12):"))
 if(1>mes or mes>12):
     print("Tienes que introducir un número entre el 1 y 12")
 else:
+    # Muestro el resultado
     print("El mes de",lst_meses[mes-1],"tiene",lst_dias[mes-1],"días.")
